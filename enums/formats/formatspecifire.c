ddc5b573b62161d184507c06d2efe2ccf7fb3dea
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-14 06:20:44 -0500

    formats

---
+++ enums/formats/formatspecifire.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+  int integerVar = 100;
+  float floatingVar = 334.79;
+  double doubleVar = 8.44e+11;
+  char charVar = 'W';
+
+  _Bool boolVar = 0;
+
+  printf("integerVar = %i\n", integerVar);
+  printf("integerVar = %f\n", floatingVar);
+  printf("integerVar = %e\n", doubleVar);
+  printf("integerVar = %g\n", doubleVar);
+  printf("integerVar = %c\n", charVar);
+  printf("integerVar = %i\n", boolVar);
+
+}
