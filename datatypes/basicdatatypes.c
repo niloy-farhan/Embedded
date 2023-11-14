238f5af3ed30dd0b0722c44b0b863733c07b1f0b
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-14 05:27:26 -0500

    datatypes

--- datatypes/basicdatatypes.c
+++ datatypes/basicdatatypes.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 int main(){
 
   float f = 23.222;
   double f1 = 55.5555555555555555e+11;
 
   _Bool boolVariable =1;
-
+  //or
+  bool b = true;
   
 }
