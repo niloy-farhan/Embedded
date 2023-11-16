07b69828f036537ec377eab5afbd074cc3d5e815
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-16 04:00:34 -0500

    bitWiseOperator

---
+++ bitwiseOperator/bitwiseOperator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+  unsigned int a = 60;
+  unsigned int b = 600;
+
+  int result = 0;
+
+  result = a | b;
+
+  printf("result is %d\n", result);
+
+  return 0;
+}
