7ac417cad88826f52d763c5d1467bb5ec149dcba
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-17 02:18:44 -0500

    initial commit

---
+++ operators/SizeOfBasicDataTypes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+  printf("Varriable of type char occupy %zd\n", sizeof(char));
+  printf("Varriable of type short occupy%zd\n", sizeof(short));
+  printf("Varriable of type int occupy%zd\n", sizeof(int));
+  printf("Varriable of type long occupy%zd\n", sizeof(long)); 
+  printf("Varriable of type long long occupy%zd\n", sizeof(long long));
+  printf("Varriable of type float occupy %zd\n", sizeof(float));
+  printf("Varriable of type double occupy %zd\n", sizeof(double));
+  printf("Varriable of type long double occupy %zd\n", sizeof(long double));
+
+  return 0;
+
+}
