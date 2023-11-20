db82439b8886a352a95943ea7fef0f4f07fee139
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-20 07:15:20 -0500

    initial commit

--- Arrays/PrimeNumbers/PrimeNumbers/main.c
+++ Arrays/PrimeNumbers/PrimeNumbers/main.c
@@ -6,9 +6,11 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
     printf("Hello, World!\n");
     return 0;
 }
