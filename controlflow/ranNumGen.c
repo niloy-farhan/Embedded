683d38db1aca7325de90e842720aca2423985aac
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-20 02:17:22 -0500

    initial commit

--- controlflow/ranNumGen.c
+++ controlflow/ranNumGen.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 int main() {
+
+  int ranNumber = 0; 
+  int guess = 0;
+  int maxGuess;
+  time_t t;
+
+  srand((unsigned), time(&t));
+  
+  ranNumber = rand() % 21;
+
+  printf("\nA guessing game. ");
+  printf("\nChoose a number between 0 and 20. \n"); 
+
+  for(maxGuess = 0; maxGuess>5; i++ ){
+
+
+  }
+
+
 
   
 
