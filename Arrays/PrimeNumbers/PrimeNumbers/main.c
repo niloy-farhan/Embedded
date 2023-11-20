c713f3b2806204f025793a833ab124943ca3858e
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-20 07:45:01 -0500

    prime

--- Arrays/PrimeNumbers/PrimeNumbers/main.c
+++ Arrays/PrimeNumbers/PrimeNumbers/main.c
@@ -9,8 +9,37 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main(int argc, const char * argv[]) {
-    
-    printf("Hello, World!\n");
+int main() {
+    int p;
+    int i;
+
+    int primes[50] = {0};
+    int primeIndex = 2;
+
+    bool isPrime;
+
+    primes[0] = 2;
+    primes[1] = 3;
+
+    for (p = 5; p <= 100; p += 2) {
+        isPrime = true;
+
+        for (i = 1; isPrime && primes[i] * primes[i] <= p; ++i) {
+            if (p % primes[i] == 0) {
+                isPrime = false;
+            }
+        }
+
+        if (isPrime) {
+            primes[primeIndex] = p;
+            ++primeIndex;
+        }
+    }
+
+
+    for (i = 0; i < primeIndex; ++i) {
+        printf("%d ", primes[i]);
+    }
+
     return 0;
 }
