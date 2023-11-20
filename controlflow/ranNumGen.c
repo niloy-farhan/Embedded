06d45b037e727c8a1c2e18e918e9897eea3160cd
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-20 02:30:15 -0500

    ranNumGame

--- controlflow/ranNumGen.c
+++ controlflow/ranNumGen.c
@@ -7,7 +7,7 @@ int main() {
 
   int ranNumber = 0; 
   int guess = 0;
-  int maxGuess;
+  int numGuess;
   time_t t;
 
   srand((unsigned), time(&t));
@@ -17,14 +17,27 @@ int main() {
   printf("\nA guessing game. ");
   printf("\nChoose a number between 0 and 20. \n"); 
 
-  for(maxGuess = 0; maxGuess>5; i++ ){
+  for(numGuess = 0; numGuess>5; numGuess++ ){
+    printf("\nYou have %d tr%s left.", numGuess, numGuess == 1 ? "y" : "ies");
+    printf("\n Enter a guess: ");
+    scanf("%d", &guess);
 
+    if(guess == ranNumber){
 
+      printf("\nCongratulations. You guessed it!\n");
+      break;
+
+    }
+    else if(guess < 0 \\ guess > 20) 
+      printf("The number is between 0 and 20 only! \n");
+    else if (ranNumber > guess){
+      printf("Sorry, %d is wrong. The number is greater than that.\n", guess);
+    }
+    else if (ranNumber < guess)
+      printf("Sorry, %d is wrong. The number is smaller than that. \n", guess)
   }
 
-
-
-  
+  printf("\n You have had 5 tries and failed, the number is %d", ranNumber);
 
   return 0;
 }
