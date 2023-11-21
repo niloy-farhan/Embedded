de386ba68ba2e9f18cf6efab5b4199fafcb997c4
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-21 03:34:36 -0500

    array

--- Arrays/arrays/arrays/main.c
+++ Arrays/arrays/arrays/main.c
@@ -9,6 +9,23 @@
 
 int main(int argc, const char * argv[]) {
     
-    printf("Hello, World!\n");
+    int grades[10];
+    int count = 10;
+    long sum = 0;
+    float avarage = 0.0f;
+    
+    printf("\nEnter the 10 Grades: \n");
+    
+    for(int i = 0; i < count; ++i){
+        printf("%u> ", i + 1 );     //%u unsigned format specifire
+        scanf("%d", &grades[i]);
+        sum += grades[i];
+    }
+    
+    avarage = (float)sum/count;
+    
+    printf("\nAvarage of the ten grades entered is: %.2f\n\n", avarage);
+    
+    
     return 0;
 }
