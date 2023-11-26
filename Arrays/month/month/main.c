fca3cc35acdd8f1b3e2a071234d4dfea19768760
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-26 03:01:57 -0500

    designated initializers

--- Arrays/month/month/main.c
+++ Arrays/month/month/main.c
@@ -9,7 +9,7 @@
 #define MONTHS 12
 
 int main(int argc, const char * argv[]) {
-    int days[MONTHS] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int days[MONTHS] = {31, [4] = 31, 30, 31, 30, 31, 31, [1] = 29};
     int index;
     
     for (index = 0; index < MONTHS; index++){
