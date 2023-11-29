91911de25a59f57e4adeeda2f04af18c077db3b8
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-29 04:00:45 -0500

    update

---
+++ Functions/greatestCommonDivisior/greatestCommonDivisior/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  greatestCommonDivisior
+//
+//  Created by Niloy Farhan on 29/11/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
