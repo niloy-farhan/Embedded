7867c314d36d5f3837f06fa0eaf703eff70030fb
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-29 04:37:51 -0500

    update

--- Functions/greatestCommonDivisior/greatestCommonDivisior/main.c
+++ Functions/greatestCommonDivisior/greatestCommonDivisior/main.c
@@ -7,7 +7,43 @@
 
 #include <stdio.h>
 
+double DivisorData(double x, double y);
+double Divisors(double z);
+
+
 int main(int argc, const char * argv[]) {
+    
+    DivisorData(10, 20);
     
     return 0;
 }
+
+double Divisors( double z){
+    for( int i = 0; i <= z; i++){
+        
+    }
+};
+
+double DivisorData(double x, double y) {
+    
+    double max = 0;
+    
+    
+    if (x > y){
+        max = x;
+    } else {
+        max = y;
+    }
+    
+    double dataX[max] = 0;
+    
+    for( double i = 0; i <= max; i++){
+        if(x % i == 0){
+            
+        }
+    }
+        
+    
+    
+};
+
