85a19c165563fd270d1d2fce018a91a9a6efd10a
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-29 05:26:58 -0500

    update

--- c programming practice/Functions/GCD Functions/GCD Functions/main.c
+++ c programming practice/Functions/GCD Functions/GCD Functions/main.c
@@ -8,7 +8,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int gcd(int u, int v);
+float absoluteValue (float x);
+
 int main(int argc, const char * argv[]) {
+    
+    int result = 0;
+    
+    result = gcd(150, 23);
+    printf("The gcd of 1500 and 25 is %d\n", result);
    
     return 0;
+}
+
+int gcd(int u, int v) {
+    int temp;
+    
+    while(v != 0){
+        temp = u % v;
+        u = v;
+        v = temp;
+    }
+    return u;
+}
+
+float absoluteValue(float x){
+    if (x < 0)
+        x = -x;
+    return x;
 }
