8ed218caee10bb461becaeff86c479c78addd459
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-29 05:33:41 -0500

    update

--- c programming practice/Functions/GCD Functions/GCD Functions/main.c
+++ c programming practice/Functions/GCD Functions/GCD Functions/main.c
@@ -10,13 +10,22 @@
 
 int gcd(int u, int v);
 float absoluteValue (float x);
+float squareRoot (float x);
 
 int main(int argc, const char * argv[]) {
     
-    int result = 0;
+    double result = 0;
+    float f1 = -15.5, f2 = 20.0, f3 = -5.0;
+    int i1 = -716;
+    float absoluteValueResult = 0.0;
+    
+    /* testing absolute value function*/
+    result = absoluteValue(f1);
+    printf("result = %.2f\n", result);
+    printf("f1 = %.2f\n", f1);
     
     result = gcd(150, 23);
-    printf("The gcd of 1500 and 25 is %d\n", result);
+    printf("The gcd of 1500 and 25 is %f\n", result);
    
     return 0;
 }
