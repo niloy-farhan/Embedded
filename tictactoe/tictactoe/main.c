1f61e6174ff25a9632817b73d028d7849425c935
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-09 02:00:45 -0500

    update

--- tictactoe/tictactoe/main.c
+++ tictactoe/tictactoe/main.c
@@ -8,6 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char square[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int choice, player;
+
+int checkForWin();
+int displayBoard();
+int markBoard(char mark);
+
 int main(int argc, const char * argv[]) {
    
     return 0;
